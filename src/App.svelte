4d921e6eb31378650fcc501aceb06e41fce77bb4
Aymen <dash.aymen@gmail.com>
2023-03-02 11:19:17 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import FeedBackList from "./componenets/FeedBackList.svelte";
+	import FeedBackStats from "./componenets/FeedBackStats.svelte";
+	import FeedBackForm from "./componenets/FeedBackForm.svelte";
+</script>
+	
+<main class="container">
+	<FeedBackForm />
+	<FeedBackStats />
+	<FeedBackList />
+</main>
+
+
+<style>
+
+</style>
---
+++ src/componenets/FeedBackList.svelte
@@ -0,0 +1,14 @@
+<script>
+    import {FeedbackStore} from '../stores'
+    import {fade, scale} from 'svelte/transition'
+    import FeedBackItem from "./FeedBackItem.svelte";
+    export let feedback = []
+
+    console.log(feedback)
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+<div in:scale out:fade>
+    <FeedBackItem item={fb}/>
+</div>
+{/each}
